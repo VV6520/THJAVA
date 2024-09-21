55da2d112f3d1f79e1ec34bba418364c2e9afced
VV6520 <Datleviet32@gmail.com>
2024-09-21 21:18:50 +0700

    baitap2-tuan3

---
+++ Xeploai.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Xeploai {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int n;
+		System.out.println("nhap vao so diem : ");
+		n=sc.nextInt();
+		if(n>=8) {
+			System.out.println("hoc sinh xep loai tot");
+		}else if(n>=6) {
+			System.out.println("hoc sinh xep loai kha");
+		}else if(n>=4) {
+			System.out.println("hoc sinh xep loai trung binh");
+		}else {
+			System.out.println("hoc sinh xep loai yeu");
+		}
+	}
+}
