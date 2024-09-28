dcbaa48b5e68fb02e19d03b1fbeed088580b3864
VV6520 <Datleviet32@gmail.com>
2024-09-28 21:42:55 +0700

    Tuan2

---
+++ baitap1t2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class baitap1t2 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String n;
+		System.out.println("nhap vao ten cua ban : ");
+		n=sc.nextLine();
+		System.out.println(" Hello "+n);
+	}
+}
