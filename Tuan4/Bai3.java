fb48ecbbcf7671caec5f1c6337df4f34b6df0027
Phùng Văn Vũ <phungvu65005@gmail.com>
2024-09-28 23:36:48 +0700

    BTTuan4

--- Tuan4/Bai3.java
+++ Tuan4/Bai3.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
 
 public class Bai3 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap mot so nguyen duong: ");
+        int n = scanner.nextInt();
+        long giaithua = 1;
+
+        for (int i = 1; i <= n; i++)
+            giaithua *= i;
+        
+            System.out.println("Giai thua cua " + n + " la: " + giaithua);
+    }
 }
