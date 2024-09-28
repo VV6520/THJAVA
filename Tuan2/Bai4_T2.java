364a93d33bb593585f572211312af49ddb4f76a2
Phùng Văn Vũ <phungvu65005@gmail.com>
2024-09-28 13:36:24 +0700

    XoaBai

--- Tuan2/Bai4_T2.java
+++ Tuan2/Bai4_T2.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+
 public class Bai4_T2 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.print("Nhap so tien gui (VND): ");
         double soTienGoc = sc.nextDouble();
         System.out.print("Nhap lai suat hang nam: ");
-        double laiSuatHangNam = sc.nextDouble(); // Nhập trực tiếp với dấu %
+        double laiSuatHangNam = sc.nextDouble();
         System.out.print("Nhap so thang gui: ");
         int soThangGui = sc.nextInt();
         sc.close();
