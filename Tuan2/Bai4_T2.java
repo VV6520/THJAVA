e0e0db6abac15a2a04e102588cbf5722aaabf5be
Phùng Văn Vũ <phungvu65005@gmail.com>
2024-09-29 21:55:40 +0700

    BTTuan2

--- Tuan2/Bai4_T2.java
+++ Tuan2/Bai4_T2.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
-public class Bai4_T2 {
+
+public class TH2_3 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Nhap so tien gui (VND): ");
-        double soTienGoc = sc.nextDouble();
-        System.out.print("Nhap lai suat hang nam: ");
-        double laiSuatHangNam = sc.nextDouble(); // Nhập trực tiếp với dấu %
-        System.out.print("Nhap so thang gui: ");
-        int soThangGui = sc.nextInt();
-        sc.close();
-        // Tính toán lãi suất đơn
-        double laiSuatHN = laiSuatHangNam / 100; // Chuyển lãi suất hàng năm sang dạng số thập phân
-        double soTienLai = soTienGoc * laiSuatHN * (soThangGui / 12.0); // Tính số tiền lãi
-        double soTienGocCuoiKy = soTienGoc + soTienLai; // Tính số tiền gốc cuối kỳ
-        // In kết quả cho lãi suất đơn
-        System.out.printf("So tien lai (lai suat don) la: %.1f VND\n", soTienLai);
-        System.out.printf("So tien goc cuoi ky (lai suat don) la: %.1f VND\n", soTienGocCuoiKy);
-        // Tính toán lãi suất kép
-        double soTienCuoiKy = soTienGoc * Math.pow(1 + laiSuatHN, soThangGui / 12.0); // Công thức tính lãi suất kép
-        double soTienLaiKep = soTienCuoiKy - soTienGoc; // Tính số tiền lãi kép
-        // In kết quả cho lãi suất kép
-        System.out.printf("So tien lai (lai suat kep) la: %.1f VND\n", soTienLaiKep);
-        System.out.printf("So tien goc cuoi ky (lai suat kep) la: %.1f VND\n", soTienCuoiKy);
+        //Nhập dữ liệu
+        System.out.printf("Nhập vào số nguyên a = ");
+        int a = new Scanner(System.in).nextInt();
+        System.out.printf("Nhập vào số nguyên b = ");
+        int b = new Scanner(System.in).nextInt();
+        //Thực hiện phép tính
+        int tong = a + b;
+        int hieu = a - b;
+        int tich = a * b;
+        double thuong = (double) a/b;
+        int du = a % b;
+        //Xuất ra màn hình
+        System.out.println("Tổng a+b = " +tong);
+        System.out.println("Hiệu a-b = " +hieu);
+        System.out.println("Tích a*b = " +tich);
+        System.out.println("Thương a/b = " +thuong);
+        System.out.println("Chia lấy dư a%b = " +du);
     }
 }
