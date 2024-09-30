e85c187f51505407a34a4d62aa00f04be987a94f
Phuoc Nguyen <nguyenphuoc0768@gmail.com>
2024-09-30 13:28:54 +0700

    Tuan 5

--- src/TH5_1.java
+++ src/TH5_1.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;
-
 public class TH5_1 {
     public static void main(String[] args) {
-        do{
-            System.out.println("Nhap vao so phan tu cua mang");
-            int n = new Scanner(System.in).nextInt();
-        }while (n<0);
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào số phần tử của mảng");
+        int n = sc.nextInt();
+        //Cấp phát bộ nhớ cho mảng
+        int[] M = new int[n];
+        //Nhập vào các phần tử mảng
+        for (int i=0; i<n; i++){
+            System.out.printf("M[ "+i+" ]= ");
+            M[i] = sc.nextInt();
+        }
+        int tong = 0;
+        for (int i=0; i<n; i++)
+            tong = tong + M[i];
+        System.out.println("Tổng các phần tử của mảng là: "+tong);
     }
 }
