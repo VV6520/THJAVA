4be9f21b84e2ee91e400da0f148e365afc3dc325
Phùng Văn Vũ <phungvu65005@gmail.com>
2024-09-30 10:26:45 +0700

    NopBai

---
+++ Tuan4/Bai4_T4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bai4_T4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap mot so nguyen: ");
+        int n = scanner.nextInt();
+        boolean KTS = n > 1;
+
+        for (int i = 2; i <= Math.sqrt(n); i++)
+            if (n % i == 0) {
+                KTS = false;
+                break;
+            }
+        if (KTS)
+            System.out.println(n + " la so nguyen to.");
+        else
+            System.out.println(n + " khong phai la so nguyen to.");
+    }
+}
---
+++ Tuan4/Bai6_T4.java
@@ -0,0 +1,5 @@
+import java.util.Scanner;
+
+public class Bai6_T4 {
+    
+}
