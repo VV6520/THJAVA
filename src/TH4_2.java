3d5764d09c9c5654053b459a60c80fe107203dfc
Nguyen Phuoc <163439962+PhuocNguyen1234@users.noreply.github.com>
2024-10-03 23:15:56 +0700

    Tuan 5

--- src/TH4_2.java
+++ src/TH4_2.java
@@ -2,9 +2,9 @@ public class TH4_2 {
     public static void main(String[] args) {
         int i, n, j, bangcc;
         n = 10;
-        for (i=1; i<n; i++){
+        for (i=2; i<n; i++){
             System.out.println("Bảng cửu chương "+i);
-            for (j=1; j<n; j++){
+            for (j=1; j<=n; j++){
                     bangcc = i*j;
                 System.out.println(i+"*"+j+" = "+bangcc);
             }
--- src/TH5_3.java
+++ src/TH5_3.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class TH5_3 {
+import java.util.Arrays;
+import java.util.Scanner;
+public class TH5_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Nhập vào số phần tử của mảng ");
+        int n = sc.nextInt();
+        //Khởi tạo mảng
+        int[] M = new int[n];
+        //Nhập dữ liệu cho mảng
+        for (int i=0; i<n; i++){
+            System.out.printf("M[ "+i+" ]= ");
+            M[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(M));
+        Arrays.sort(M);
+        System.out.println("Mảng sau khi sắp xếp là: "+Arrays.toString(M));
+    }
 }
